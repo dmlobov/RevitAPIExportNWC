26fc1b9e3a8791cb725534b38a0d8d72ee849189
D L <d.m.lobov@yandex.ru>
2023-02-15 18:19:32 +0300

    Добавьте файлы проекта.

---
+++ Main.cs
@@ -0,0 +1,25 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPIExportNWC
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            Document doc = commandData.Application.ActiveUIDocument.Document;
+
+            var nwcOption = new NavisworksExportOptions();
+            doc.Export(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "export.nwc", nwcOption);
+
+            return Result.Succeeded;
+        }
+    }
+}
